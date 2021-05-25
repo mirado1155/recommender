4037efbc5600368def4727c646bc44cf92d20cfc
Robert Adams <radams8@madisoncollege.edu>
2021-05-25 10:42:29 -0500

    removed unused prop

--- components/CategoryItem.js
+++ components/CategoryItem.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-const CategoryItem =({category, items}) => {
+const CategoryItem =({category}) => {
     return (
         <Link href="/recommender/[category]" as={`recommender/${category}`}>
             <a className={styles.card}>
