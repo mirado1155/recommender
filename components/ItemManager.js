a361d40b3a0dc1fcb4047ee3a5f555e43c5aeadd
Robert Adams <radams8@madisoncollege.edu>
2021-05-25 14:32:21 -0500

    extracted item manager into separate component

---
+++ components/ItemManager.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react'
+import IndividualItem from '../components/IndividualItem'
+
+const ItemManager = ({categoryItems, cat, itemUpdater}) => {
+    const [items, setItems] = useState()
+    const [toAdd, setToAdd] = useState("")
+
+    //Handles a click for either adding or removing an item
+    const handleClick = event => {
+        let name = event.target.name
+        let action = event.target.value
+        manageItem(name, action)
+    }
+
+    //Handles a keyup specifically for adding an item
+    const handleKeyup = event => {
+        let name = event.target.value
+        let action = "add"
+        event.keyCode == 13 ? manageItem(name, action) : null
+    }
+
+    //updates state with form input content so that it can be removed after 'submission'
+    const updateAdd = event => {
+        setToAdd(event.target.value)
+    }
+
+    //Deals with adding and removing item from server
+    const manageItem = async (name, action) => {
+
+        const res = await fetch (`http://192.168.0.103:3000/categories/${cat}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name: `${name}`,
+                action: `${action}`
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        const confirmation = await res.json()
+        console.log(confirmation)
+        res.status < 300 ? updateItemsList(name, action) : console.error("Could not fulfill request")
+        
+    }
+
+    const updateItemsList = (name, action) => {
+        let itemsToUpdate = items
+        action == "add" ? itemsToUpdate.push(name) : null 
+        action == "remove" ? itemsToUpdate.splice(itemsToUpdate.indexOf(name), 1) : null
+        setItems([...itemsToUpdate])
+        setToAdd("")
+    }
+
+
+    let itemsList
+
+
+    !items ? setItems(categoryItems) : itemsList = items.map(item => {
+        return <IndividualItem item={item} removeItem={handleClick} />
+    });
+
+    return (
+        <section id="itemManager">
+                <h2>{cat}</h2>
+
+                    <label htmlFor="toAdd">Add Item: </label>
+                    <input type="text" id="toAdd" name={toAdd} value={toAdd} onChange={updateAdd} onKeyUp={handleKeyup}></input>
+                    <button onClick={handleClick} value="add" name={toAdd}>Add Item</button>
+
+                <div className="itemViewer">
+                <ul>
+                    {items ? itemsList.map(item => {
+                        return item;
+                    }) : null}
+                </ul>
+                </div>
+        </section>
+
+    )
+}
+
+export default ItemManager
--- pages/recommender/[category]/index.js
+++ pages/recommender/[category]/index.js
@@ -1,73 +1,16 @@
-import React, { Component, useEffect, useState, componentDidMount } from 'react'
+import React, { useState } from 'react'
 import IndividualItem from '../../../components/IndividualItem'
+import ItemManager from '../../../components/ItemManager'
 
 const category = ({categoryItems, cat}) => {
+    const [pageItems, setPageItems] = useState([])
 
-    
-    const [items, setItems] = useState(0)
-    const [toAdd, setToAdd] = useState(0)
-
-    //Handles a click for either adding or removing an item
-    const handleClick = event => {
-        let name = event.target.name
-        let action = event.target.value
-        manageItem(name, action)
+    const updateItems = items => {
+        setPageItems([...items])
+        console.log(items)
     }
-
-    //Handles a keyup specifically for adding an item
-    const handleKeyup = event => {
-        let name = event.target.value
-        let action = "add"
-        event.keyCode == 13 ? manageItem(name, action) : null
-    }
-
-    //updates state with form input content so that it can be removed after 'submission'
-    const updateAdd = event => {
-        setToAdd(event.target.value)
-    }
-
-    //Deals with adding and removing item from server
-    const manageItem = async (name, action) => {
-
-        const res = await fetch (`http://192.168.0.103:3000/categories/${cat}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                name: `${name}`,
-                action: `${action}`
-            }),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        })
-        const confirmation = await res.json()
-        console.log(confirmation)
-        window.location.reload()
-        setToAdd("")
-    }
-
-
-    let itemsList
-
-    !items ? setItems(categoryItems) : itemsList = items.map(item => {
-        return <IndividualItem item={item} removeItem={handleClick} />
-    });
-
     return (
-        <div>
-            <h2>{cat}</h2>
-
-                <label htmlFor="toAdd">Add Item: </label>
-                <input type="text" id="toAdd" name={toAdd} value={toAdd} onChange={updateAdd} onKeyUp={handleKeyup}></input>
-                <button onClick={handleClick} value="add" name={toAdd}>Add Item</button>
-
-            <div className="itemViewer">
-            <ul>
-                {items ? itemsList.map(item => {
-                    return item;
-                }) : null}
-            </ul>
-            </div>
-        </div>
+        <ItemManager categoryItems={categoryItems} cat={cat} itemUpdater={updateItems} />
     )
 }
 
