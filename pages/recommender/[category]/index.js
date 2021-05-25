09374a4479751010ca62f2c88991a1a28d9fbfd3
Robert Adams <radams8@madisoncollege.edu>
2021-05-25 16:02:43 -0500

    adding custom styles

--- pages/recommender/[category]/index.js
+++ pages/recommender/[category]/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Recommender from '../../../components/Recommender'
 import ItemManager from '../../../components/ItemManager'
 import Link from 'next/link'
+import styles from '../../../styles/Home.module.css'
 
 const category = ({categoryItems, cat}) => {
     const [pageItems, setPageItems] = useState()
@@ -14,11 +15,13 @@ const category = ({categoryItems, cat}) => {
     
     console.log(pageItems)
     return (
-        <div>
-            <h2>{cat}</h2>
-            <Link href="/">
-                <button>Go Back</button>
-            </Link>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h2 className={styles.title}>{cat}</h2>
+                <Link href="/">
+                    <button className={styles.button}>Go Back</button>
+                </Link>
+            </header>
             <ItemManager categoryItems={categoryItems} cat={cat} updatePageItems={updatePageItems} />
             <Recommender items={pageItems} />
         </div>
