1a978e33d7442bbf0ed2d8bb10b3d7ce7b8f4c8f
Robert Adams <radams8@madisoncollege.edu>
2021-05-25 16:08:57 -0500

    custom styles to manager section

--- components/IndividualItem.js
+++ components/IndividualItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from '../styles/Home.module.css'
 
 const IndividualItem = ({item, removeItem}) => {
 
     return (
         <div>
-            <p>{item} <button onClick={removeItem} name={item} value="remove">X</button></p>
+            <p className={styles.itemText}>{item} <button className={styles.removeButton} onClick={removeItem} name={item} value="remove">X</button></p>
         </div>
     );
 }
